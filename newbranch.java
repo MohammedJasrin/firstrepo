4d4ca06f7651a2371ac2d43e3f917be6d32799c6
MohammedJasrin <mohammedjasrinmj@gmail.com>
2024-03-05 12:34:54 +0530

    added hello

--- newbranch.java
+++ newbranch.java
@@ -3,6 +3,6 @@ public class Demo
   public static void main([String args])
   {
     System.out.println("First branch");
-    System.out.println("Mohammed Jasrin")
+    System.out.println("Hello")
   }
 }
